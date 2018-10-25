345c7658d1fd64fae1ad0dbae2eae152b036ac67
igorryabtsev <igorryabtsev@inbox.ru>
2018-10-25 11:47:50 +0300

    Some refactoring

--- src/main/kotlin/SimplePSIVisitor.kt
+++ src/main/kotlin/SimplePSIVisitor.kt
@@ -7,22 +7,18 @@ class SimplePSIVisitor : JavaRecursiveElementVisitor() {
 
     var methodAndExpressions: HashMap<PsiMethod, ArrayList<PsiMethodCallExpression>> = HashMap()
 
-    override fun visitMethod(method: PsiMethod?) {
-        if (method != null) {
-            methodAndExpressions[method] = ArrayList()
-        }
+    override fun visitMethod(method: PsiMethod) {
+        methodAndExpressions[method] = ArrayList()
         super.visitMethod(method)
     }
 
-    override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
-        if (expression != null) {
-            val mainMethod = PsiTreeUtil.getParentOfType(expression.context, PsiMethod::class.java)
-            if (mainMethod != null) {
-                val expressions = methodAndExpressions[mainMethod]
-                expressions!!.add(expression)
-            }
+    override fun visitMethodCallExpression(expression: PsiMethodCallExpression) {
+        val mainMethod = PsiTreeUtil.getParentOfType(expression.context, PsiMethod::class.java)
+        if (mainMethod != null) {
+            val expressions = methodAndExpressions[mainMethod]
+            expressions!!.add(expression)
         }
         super.visitMethodCallExpression(expression)
     }
-
+// убрать ?
 }
