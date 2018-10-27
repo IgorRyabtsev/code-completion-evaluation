9b2504a0ded15f77460f56b26da51dac1d68e47c
vasyoid <vasyoid@gmail.com>
2018-10-27 16:46:20 +0300

    Update group id and create sample src directory

---
+++ antlr-parser/src/main/resources/samples/SampleJavaClass.java
@@ -0,0 +1,10 @@
+package samples;
+
+import static java.lang.Math.min;
+
+public class SampleJavaClass {
+
+    public static void main(String[] args) {
+        System.out.println(min(3, 4));
+    }
+}
