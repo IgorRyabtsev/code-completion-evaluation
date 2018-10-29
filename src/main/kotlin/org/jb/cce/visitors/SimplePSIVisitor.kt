e28b14f5e028bf873480abc7a0db83db6f06c1a1
igorryabtsev <igorryabtsev@inbox.ru>
2018-10-29 18:16:52 +0300

    Added field to visitor, to recognize necessary class

--- src/main/kotlin/org/jb/cce/visitors/SimplePSIVisitor.kt
+++ src/main/kotlin/org/jb/cce/visitors/SimplePSIVisitor.kt
@@ -1,33 +1,27 @@
 package org.jb.cce.visitors
 
 import com.intellij.psi.JavaRecursiveElementVisitor
-import com.intellij.psi.PsiAnonymousClass
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.util.PsiTreeUtil
 
-class SimplePSIVisitor : JavaRecursiveElementVisitor() {
+class SimplePSIVisitor(private val targetClass: PsiClass) : JavaRecursiveElementVisitor() {
 
-    private val methodAndExpressions: MutableMap<PsiMethod, MutableList<PsiMethodCallExpression>> = mutableMapOf()
+    val methodToExpressions: MutableMap<PsiMethod, MutableList<PsiMethodCallExpression>> = mutableMapOf()
 
     override fun visitMethod(method: PsiMethod) {
-        if (method.context !is PsiAnonymousClass) {
-            methodAndExpressions[method] = ArrayList()
+        if (method.parent == targetClass) {
+            methodToExpressions[method] = ArrayList()
+            super.visitMethod(method)
         }
-        super.visitMethod(method)
     }
 
     override fun visitMethodCallExpression(expression: PsiMethodCallExpression) {
-        var parentNodeOfExpression = expression.parent
-        var mainMethod = PsiTreeUtil.getParentOfType(parentNodeOfExpression, PsiMethod::class.java)
+        val mainMethod = PsiTreeUtil.getParentOfType(expression.parent, PsiMethod::class.java)
         if (mainMethod != null) {
-            var expressions = methodAndExpressions[mainMethod]
-            while (expressions == null) {
-                parentNodeOfExpression = parentNodeOfExpression.parent
-                mainMethod = PsiTreeUtil.getParentOfType(parentNodeOfExpression, PsiMethod::class.java)
-                expressions = methodAndExpressions[mainMethod]
-            }
-            expressions.add(expression)
+            val expressions = methodToExpressions[mainMethod]
+            expressions!!.add(expression)
         }
         super.visitMethodCallExpression(expression)
     }
